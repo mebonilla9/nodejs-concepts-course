dcde7535be9592b5caed3baa7380f0d22d6c6796
Manuel Ernesto Bonilla Muñnoz <mebonilla9@gmail.com>
2024-08-18 19:31:15 -0500

    feat: Build first steps examples

---
+++ first-steps/index.js
@@ -0,0 +1,17 @@
+/**
+ * Basic Instruction to print in the js console
+ * 
+ * To run the script -> node index.js
+ */
+console.log('Hola, Mundo 👏🏻')
+console.info('Hola, Mundo!')
+console.error('Hola, Mundo!')
+
+/**
+ * Global objects in node
+ * window object not exist in node.js
+ * Use globalThis
+ */
+
+console.log(globalThis)
+globalThis.console.log('Hello')
